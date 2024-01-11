ca11c2c10fe27aa075ab585d45f1e21003327e36
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-01-11 12:03:05 +0100

    chapter 4 - Backend sichern

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,18 +1,25 @@
 import dbConnect from "../../../db/connect";
 import Place from "../../../db/models/Place";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(request, response) {
   await dbConnect();
+  const session = await getServerSession(request, response, authOptions);
 
   if (request.method === "GET") {
     const places = await Place.find();
     return response.status(200).json(places);
   } else if (request.method === "POST") {
     try {
+      if (session){
       const placeData = request.body;
       const place = new Place(placeData);
       await place.save();
       response.status(201).json({ status: "Place created" });
+      }else{
+        response.status(401).json({ status: "Not authorized" });
+      }
     } catch (error) {
       console.error(error);
       response.status(400).json({ error: error.message });
